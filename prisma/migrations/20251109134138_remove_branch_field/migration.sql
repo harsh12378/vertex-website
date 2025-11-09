e51266adb3f3cca94ff6233b15127e4197eba124
harsh12378 <harshpandey12378@gmail.com>
2025-11-09 21:54:45 +0530

    error 4

--- prisma/migrations/20251109134138_remove_branch_field/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `branch` on the `recruitments` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "public"."recruitments" DROP COLUMN "branch";
